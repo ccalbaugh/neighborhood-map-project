3208e1f26c3c6ba11bc95c044e1efc53d68f0fa3
Caden Albaugh <CadenAlbaugh@gmail.com>
2016-05-25 16:33:30 -0500

    added an Ajax wiki call for all places and deleted an ancillary file

--- js/view/app.js
+++ js/view/app.js
@@ -1,215 +1,256 @@
-(function(global, mapster, $) {
+// Model
 
-	var places = [
-		{
-			title: 'The Englert Theater',
-			id: 'ChIJSyeNzu9B5IcRQzciSCOPPuk',  // A Google Places ID.
-			formatted_address: '221 E Washington St, Iowa City, IA 52240',
-			// latLng: {lat: 41.659794, lng: -91.532322},
-			marker: "",
-			lat: 41.659794,
-			lng: -91.532322
-		},
-		{
-			title: 'Basta Pizzeria Ristorante',
-			id: 'ChIJn6fO2fFB5IcR6jVX9EN9K6A',  // A Google Places id.
-			formatted_address: '121 Iowa Ave, Iowa City, IA 52240',
-			// latLng: {lat: 41.661022, lng: -91.533625},
-			lat: 41.661022,
-			lng: -91.533625
-		},
-		{
-			title: 'Donnellys Irish Pub',
-			id: 'ChIJgxklQ-5B5IcRjdC81k4t4ug',  // A Google Places id.
-			formatted_address: '110 E College St, Iowa City, IA 52240',
-			// latLng: {lat: 41.659075, lng: -91.534083},
-			lat: 41.659794,
-			lng: -91.532322
-		}, 
-		{
-			title: 'Shorts Burger & Shine',
-			id: 'ChIJEactIO5B5IcRJ6QdjEyd_UI',  // A Google Places id.
-			formatted_address: '18 S Clinton St, Iowa City, IA 52240',
-			// latLng: {lat: 41.660629, lng: -91.534355},
-			lat: 41.659794,
-			lng: -91.532322
-		}, 
-		{
-			title: 'FilmScene',
-			id: 'ChIJ9WOyRe5B5IcRgFg8TDssBz8',  // A Google Places id.
-			formatted_address: '118 E College St, Iowa City, IA 52240',
-			// latLng: {lat: 41.659283, lng: -91.533779},
-			lat: 41.659794,
-			lng: -91.532322
-		},
-		{
-			title: 'Blue Moose Inc',
-			id: 'ChIJMWk1KPBB5IcRXC6hLJYE0Uo',  // A Google Places id.
-			formatted_address: '211 Iowa Ave, Iowa City, IA 52240',
-			// latLng: {lat: 41.661003, lng: -91.532341},
-			lat: 41.661003,
-			lng: -91.533541
-		},
-		{
-			title: 'The Mill Restaurant',
-			id: 'ChIJUQ219u5B5IcRBOsWmmNQJnk',  // A Google Places id.
-			formatted_address: '120 E Burlington St, Iowa City, IA 52240',
-			// latLng: {lat: 41.658244, lng: -91.533651},
-			lat: 41.658244,
-			lng: -91.533651
-		},
-	];
+var places = [
+	{
+		title: 'The Englert Theater',
+		id: 'ChIJSyeNzu9B5IcRQzciSCOPPuk',  // A Google Places ID.
+		formatted_address: '221 E Washington St, Iowa City, IA 52240',
+		latLng: {lat: 41.659794, lng: -91.532322},
+		marker: "",
+		lat: 41.659794,
+		lng: -91.532322,
+		url: ""
+	},
+	{
+		title: 'Basta Pizzeria Ristorante',
+		id: 'ChIJn6fO2fFB5IcR6jVX9EN9K6A',  // A Google Places id.
+		formatted_address: '121 Iowa Ave, Iowa City, IA 52240',
+		latLng: {lat: 41.661022, lng: -91.533625},
+		lat: 41.661022,
+		lng: -91.533625,
+		url: ""
+	},
+	{
+		title: 'Donnellys Irish Pub',
+		id: 'ChIJgxklQ-5B5IcRjdC81k4t4ug',  // A Google Places id.
+		formatted_address: '110 E College St, Iowa City, IA 52240',
+		latLng: {lat: 41.659075, lng: -91.534083},
+		lat: 41.659794,
+		lng: -91.532322,
+		url: ""
+	}, 
+	{
+		title: 'Shorts Burger & Shine',
+		id: 'ChIJEactIO5B5IcRJ6QdjEyd_UI',  // A Google Places id.
+		formatted_address: '18 S Clinton St, Iowa City, IA 52240',
+		latLng: {lat: 41.660629, lng: -91.534355},
+		lat: 41.659794,
+		lng: -91.532322,
+		url: ""
+	}, 
+	{
+		title: 'FilmScene',
+		id: 'ChIJ9WOyRe5B5IcRgFg8TDssBz8',  // A Google Places id.
+		formatted_address: '118 E College St, Iowa City, IA 52240',
+		latLng: {lat: 41.659283, lng: -91.533779},
+		lat: 41.659794,
+		lng: -91.532322,
+		url: ""
+	},
+	{
+		title: 'Blue Moose Inc',
+		id: 'ChIJMWk1KPBB5IcRXC6hLJYE0Uo',  // A Google Places id.
+		formatted_address: '211 Iowa Ave, Iowa City, IA 52240',
+		latLng: {lat: 41.661003, lng: -91.532341},
+		lat: 41.661003,
+		lng: -91.533541,
+		url: ""
+	},
+	{
+		title: 'The Mill Restaurant',
+		id: 'ChIJUQ219u5B5IcRBOsWmmNQJnk',  // A Google Places id.
+		formatted_address: '120 E Burlington St, Iowa City, IA 52240',
+		latLng: {lat: 41.658244, lng: -91.533651},
+		lat: 41.658244,
+		lng: -91.533651,
+		url: ""
+	},
+];
 
-	var Place = function(data) {
-		this.address = ko.observable(data.address);
-		this.latLng = ko.observable(data.latLng);
-		this.title = ko.observable(data.title);
-		this.marker = ko.observable(0);
+
+var ViewModel = function() {
+	var self = this;
+
+	var placeInfo = function(data) {
+		self.title = ko.observable(data.title);
+		self.address = ko.observable(data.address);
+		self.lat = ko.observable(data.lat);
+		self.lng = ko.observable(data.lng);
+		self.latLng = ko.computed(function () {
+			return self.lat() + self.lng();
+		});
 	};
 
-	function initMap() {
-		var options = mapster.MAP_OPTIONS,
-		element = document.querySelector('#map-canvas'),
-		map = mapster.create(element, options);
 
-		service = new google.maps.places.PlacesService(map);
+	self.currentLocation = ko.observable();
+	self.allPlaces = ko.observableArray(places);
 
-		ko.applyBindings( new ViewModel() );
+	places.forEach(function(place) {
+		self.allPlaces.push(place);
+	});
+
+	console.log(self.allPlaces);
+	console.log('***' + self.allPlaces());
+
+	self.setLocation = function(clickedLocation) {
+		self.currentLocation(clickedLocation);
+	};
+
+	function drop() {
+		for (var i = 0; i < self.allPlaces().length; i++) {
+			addMarker(self.allPlaces()[i], i * 400);
+		}	
 	}
 
-	var ViewModel = function() {
-		var self = this;
+	function addMarker(place, timeout) {
+		setTimeout(function() {
+			place.marker = new google.maps.Marker({
+				map: map,
+				position: place.latLng,
+				title: place.title,
+				animation: google.maps.Animation.DROP
+			});	
 
-		self.userInput = ko.observable();
-		self.currentLocation = ko.observable();
-		self.allPlaces = [];
-		self.visiblePlaces = ko.observableArray([]);
+			console.log(place.lat + place.lng);
 
-		places.forEach(function(place) {
-			self.visiblePlaces().push(place);
-		});
+			if (!place.infoWindow) {
+				place.infoWindow = new google.maps.InfoWindow();
+			}
 
-		places.forEach(function(place) {
-			self.allPlaces.push(place);
-		});
+			// Initialize the vars for the Wikipedia API, thanks to Cshields88 on Github for the api layout
+			var content,
+				urlNames = encodeURI(place.title),
+				wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" +
+							 urlNames + "&limit=1&redirects=return&format=json";
 
-		// animates the correct marker
-		self.markerClick = function(place) {
-			google.maps.event.trigger(place.marker, 'click');
-		};
-		
-		self.setLocation = function(clickedLocation) {
-			self.currentLocation(clickedLocation);
-		};
+			self.apiTimeout = setTimeout(function() {
+				//alert('ERROR: Failed to load data');
+			}, 5000);
 
-		function drop() {
-			for (var i = 0; i < self.visiblePlaces().length; i++) {
-				addMarkerWithTimeout(self.visiblePlaces()[i], i * 400);
-			}	
-		}
-
-		function addMarkerWithTimeout(place, timeout) {
-			setTimeout(function() {
-				var marker = map.addMarker({
-					lat: place.lat,
-					lng: place.lng,
-					events: [{
-						name: 'click',
-						callback: selectMarker(place, marker, place.infoWindow)
-					}],
-				 content: '<div class="infoWindow"><strong>' + place.title + '</strong><br>' +
-		         			'Place ID: ' + place.id + '<br>' +
-		         			place.formatted_address + '</div>',
-					icon: 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-128e4d/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/tree.png'
-				});
-
-//////////////////  OLD CODE
-				// place.marker = new google.maps.Marker({
-				// 	position: {
-				// 		lat: place.lat,
-				// 		lng: place.lng
-				// 	},
-				// 	map: map,
-				// 	icon: 'https://mapicons.mapsmarker.com/wp-content/uploads/mapicons/shape-default/color-128e4d/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/tree.png',
-				// 	animation: google.maps.Animation.DROP,
-				// 	title: place.title
-				// });	
-
-				place.infoWindow = new google.maps.InfoWindow({
-					content: '<div class="infoWindow"><strong>' + place.title + '</strong><br>' +
-		          				'Place ID: ' + place.id + '<br>' +
-		          				place.formatted_address + '</div>'
-				});			
-
-				google.maps.event.addListener(place.marker, 'click', selectMarker(place, place.marker, place.infoWindow));
-
-			}, timeout);
-		}
-
-		// run drop() after the map is idle, which should signifiy that it is fully loaded
-		google.maps.event.addListenerOnce(map, 'idle', drop);
-
-		
-	  	var lastInfoWindow = null;
-
-		function selectMarker(place, marker, infoWindow) {
-			function toggleBounce(marker) {
-				if (marker.getAnimation() !== null) {
-					setTimeout(function() { marker.setAnimation(null); }, 750);
-				} else {
-					// Sets the marker to bounce twice and then stop, thanks to Simon Steinberger on stackoverflow for the solution
-					marker.setAnimation(google.maps.Animation.BOUNCE);
-					setTimeout(function() { marker.setAnimation(null); }, 750);
-					self.setLocation(marker);
-				}
-			};
-			// this function will 
-			return function() {
-
-				if (lastInfoWindow === infoWindow) {
-					toggleBounce(marker);
-					currentLocation = null;
-					infoWindow.close(map, this);
-					lastInfoWindow = null;
-				} else {
-
-					if (lastInfoWindow !== null) {
-						lastInfoWindow.close(map, this);
-						toggleBounce(marker);
+			$.ajax({
+				url: wikiURL,
+				dataType: "jsonp",
+				success: function(response) {
+					clearTimeout(self.apiTimeout);
+					var articleList = response[1];
+					console.log(response);
+					if (articleList.length > 0) {
+						for (var i = 0; i < articleList.length; i++) {
+							var articleStr = articleList[i];
+							var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+							content = '<div class="infoWindow"><strong>' + place.title + '</strong><br>' +
+										'<p>' + place.formatted_address + '</p>' +
+										'<p>' + response[2] + '</p>' + // response[2] for more modern response
+										'<a href="' + place.url + '" target="_blank">' +
+										place.url + '</a>' + 
+							'</div>';
+							place.infoWindow.setContent(content);
+						}
+					} else {
+						content = '<div class="infoWindow"><strong>' + place.title + '</strong><br>' +
+										'<p>' + place.formatted_address + '</p>' +
+										'<p>' + "Sorry, No articles were found on Wikipedia" + '</p>' + 
+						'</div>';
+						place.infoWindow.setContent(content);
 					}
+				},
+				error: (function () {
+					content = '<div class="infoWindow"><strong>' + place.title + '</strong><br>' +
+									'<p>' + place.formatted_address + '</p>' +
+									'<p>' + "Failed to reach Wikipedia Servers, please try again..." + '</p>' + 
+					'</div>';
+					place.infoWindow.setContent(content);
+				})
+			}); // end of ajax call		
 
-				toggleBounce(marker);	
-		        infoWindow.open(map.gMap, this);
-				lastInfoWindow = infoWindow;
+			google.maps.event.addListener(place.marker, 'click', selectMarker(place, place.marker, place.infoWindow));
 
-				map.panTo(place.latLng);
+		}, timeout);
+	}
+
+	// run drop() after the map is idle, which should signifiy that it is fully loaded
+	google.maps.event.addListenerOnce(map, 'idle', drop);
+
+  	var lastInfoWindow = null;
+
+	function selectMarker(place, marker, infoWindow) {
+		function toggleBounce(marker) {
+			if (marker.getAnimation() !== null) {
+				setTimeout(function() { marker.setAnimation(null); }, 1500);
+			} else {
+				// Sets the marker to bounce twice and then stop, thanks to Simon Steinberger on stackoverflow for the solution
+				marker.setAnimation(google.maps.Animation.BOUNCE);
+				setTimeout(function() { marker.setAnimation(null); }, 1500);
+				self.setLocation(marker);
+			}
+		};
+		
+		return function() {
+
+			if (lastInfoWindow === infoWindow) {
+				toggleBounce(marker);
+				currentLocation = null;
+				infoWindow.close(map, this);
+				lastInfoWindow = null;
+			} else {
+
+				if (lastInfoWindow !== null) {
+					lastInfoWindow.close(map, this);
+					toggleBounce(marker);
 				}
+
+			toggleBounce(marker);	
+	        infoWindow.open(map, this);
+			lastInfoWindow = infoWindow;
+
+			map.panTo(place.latLng);
 			}
 		}
+	}
+	// animates the correct marker
+	self.list = function(place, marker) {
+		google.maps.event.trigger(place.marker, 'click');
+	};
+    // used for Search functionality with local names
+	self.userInput = ko.observable('');
 
-		self.filterMarkers = function() {
-			var searchInput = self.userInput().toLowerCase();
-			// clears the observable array so the list can be repopulated with matching results
-			self.visiblePlaces.removeAll();
-			// visiblePlaces is used for the list of names, and allPlaces is used for the markers
-			self.allPlaces.forEach(function(place) {
-				place.marker.setVisible(false);
-
-				if (place.title.toLowerCase().indexOf(searchInput) !== -1) {
-					self.visiblePlaces.push(place);
-				}
-			});
-
-			self.visiblePlaces().forEach(function(place) {
+	self.searchResults = ko.computed(function() {
+		return ko.utils.arrayFilter(self.allPlaces(), function(place) {
+			var listFilter = place.title.toLowerCase().indexOf(self.userInput().toLowerCase()) !== -1;
+			if (listFilter) {
 				place.marker.setVisible(true);
-			});
-		};
+			} else {
+				place.marker.setVisible(false);
+			}
+
+			return listFilter;
+		});
+	});
+
+	
+};
+
+// Where the google Map is created
+function initMap() {
+	options = {
+			center: {lat: 41.659794, lng: -91.532322},
+			zoom: 16,
+			disableDefaultUI: false,
+			scrollwheel: false,
+			draggable: false,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
-	initMap(mapster);
+	element = document.querySelector('#map-canvas');
+	map = new google.maps.Map(element, options);
+	service = new google.maps.places.PlacesService(map);
 
-	// creates an alert if the Google Maps API can't be reached
-	function googleError() { alert("Sorry! Google Maps cannot be loaded"); }
+	ko.applyBindings( new ViewModel() );
+}
 
-}(this, this.Mapster || (this.Mapster = {}), this.jQuery));
+// creates an alert if the Google Maps API can't be reached
+function googleError(e) { 
+	alert("Sorry! Google Maps cannot be loaded at this time");
+	console.log(e);
+}
 
